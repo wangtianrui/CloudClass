fffa7caf944aeae3d099915f7b84d2ed4fcd5140
ScorpioMiku <1056992492@qq.com>
2019-06-22 13:52:17 +0800

    butter glide

--- app/src/main/java/com/scorpiomiku/cloudclass/MainActivity.java
+++ app/src/main/java/com/scorpiomiku/cloudclass/MainActivity.java
@@ -1,13 +1,24 @@
- package com.scorpiomiku.cloudclass;
+package com.scorpiomiku.cloudclass;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.test)
+    ImageView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Glide.with(this).load(R.drawable.ic_launcher_background).into(test);
     }
 }
