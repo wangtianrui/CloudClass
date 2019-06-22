c2aef87bfd8b17e4ab1663f372a76c9886ae52e0
ScorpioMiku <1056992492@qq.com>
2019-06-22 14:13:11 +0800

    libs

--- app/src/main/java/com/scorpiomiku/cloudclass/LoginActivity.java
+++ app/src/main/java/com/scorpiomiku/cloudclass/LoginActivity.java
@@ -9,16 +9,13 @@ import com.bumptech.glide.Glide;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
-
-    @BindView(R.id.test)
-    ImageView test;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
-        Glide.with(this).load(R.drawable.ic_launcher_background).into(test);
+
     }
 }
